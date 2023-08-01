43cbdf1c916f1117d7be6674ce72f4b434814448
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-08-01 22:55:38 +0100

    Allow window state updates on different keystrokes

--- app.go
+++ app.go
@@ -3,7 +3,14 @@ package main
 import (
 	"context"
 	"fmt"
+
+	"github.com/wailsapp/wails/v2/pkg/runtime"
+	"github.com/nsf/termbox-go"
+	"github.com/pterm/pterm"
 )
+
+var currentWindowStateOpen bool
+
 type App struct {
 	ctx context.Context
 }
@@ -11,8 +18,33 @@ func GoSearch() *App {
 	return &App{}
 }
 
+func keystrokeListener(ctx context.Context) {
+	err := termbox.Init()
+	if err != nil {
+		pterm.Fatal.WithFatal(true).Println(err)
+	}
+	defer termbox.Close()
+
+	for {
+		event := termbox.PollEvent()
+		if event.Type == termbox.EventKey {
+			if event.Key == termbox.KeyEsc {
+				if currentWindowStateOpen == true {
+					runtime.Hide(ctx)
+					currentWindowStateOpen = false
+				}
+			} else if event.Key == termbox.KeyF5 {
+				runtime.Show(ctx)
+				currentWindowStateOpen = true
+			}
+		}
+	}
+}
+
 func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
+
+	go keystrokeListener(a.ctx)
 }
 
 
