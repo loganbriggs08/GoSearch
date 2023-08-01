9d3dbfc33548dd484ea6bbf686f317c3a025386d
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-08-01 18:25:45 +0100

    Update function name

--- app.go
+++ app.go
@@ -9,9 +9,7 @@ import (
 type App struct {
 	ctx context.Context
 }
-
-// NewApp creates a new App application struct
-func NewApp() *App {
+func GoSearch() *App {
 	return &App{}
 }
 
@@ -21,7 +19,6 @@ func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
 }
 
-// Greet returns a greeting for the given name
-func (a *App) Greet(name string) string {
-	return fmt.Sprintf("Hello %s, It's show time!", name)
+func (a *App) Search(name string) {
+	fmt.Printf("Search Result: %s", name)
 }
