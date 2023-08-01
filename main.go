0a06fc0d93007a63c7f2f4bdc4f3e2a131894e59
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-08-01 15:19:56 +0100

    Make window translucent

--- main.go
+++ main.go
@@ -6,16 +6,15 @@ import (
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/options/windows"
 )
 
 //go:embed all:frontend/dist
 var assets embed.FS
 
 func main() {
-	// Create an instance of the app structure
 	app := NewApp()
 
-	// Create application with options
 	err := wails.Run(&options.App{
 		Title:  "GoSearch",
 		Width:  1024,
@@ -23,7 +22,10 @@ func main() {
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
-		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		Windows: &windows.Options{
+			WindowIsTranslucent: true,
+		},
+//		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
 		OnStartup:        app.startup,
 		Bind: []interface{}{
 			app,
