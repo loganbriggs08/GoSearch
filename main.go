754c53894c04ac071772f5409c0cf8cd42e14fdf
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-08-01 17:10:42 +0100

    Remove old comments

--- main.go
+++ main.go
@@ -8,14 +8,11 @@ import (
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
 )
 
-//go:embed all:frontend/dist
 var assets embed.FS
 
 func main() {
-	// Create an instance of the app structure
 	app := NewApp()
 
-	// Create application with options
 	err := wails.Run(&options.App{
 		Title:  "GoSearch",
 		Width:  1024,
