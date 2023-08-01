63e2fe980b4ca19c9e7d98a05d6d8a0b72190aa8
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-08-01 18:23:12 +0100

    Greet -> Search

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,20 @@
 import {useState} from 'react';
 import logo from './assets/images/logo-universal.png';
 import './App.css';
-import {Greet} from "../wailsjs/go/main/App";
+import {Search} from "../wailsjs/go/main/App";
 
 function App() {
-    const [resultText, setResultText] = useState("Please enter your name below 👇");
     const [name, setName] = useState('');
     const updateName = (e: any) => setName(e.target.value);
-    const updateResultText = (result: string) => setResultText(result);
 
     function greet() {
-        Greet(name).then(updateResultText);
+        Search(name);
     }
 
     return (
         <div id="App">
-            <div id="result" className="result">{resultText}</div>
             <div id="input" className="input-box">
-                <input id="name" className="input" onChange={updateName} autoComplete="off" name="input" type="text"/>
+                <input id="name" className="input" onChange={updateName} name="input" type="text"/>
                 <button className="btn" onClick={greet}>Greet</button>
             </div>
         </div>
