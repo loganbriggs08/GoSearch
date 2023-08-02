4e2ebee3f2656f32adb4c77b7cf4d8566ae3973c
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-08-02 14:38:21 +0100

    Update frontend and css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,20 +2,28 @@ import './App.css';
 import {Search} from "../wailsjs/go/main/App";
 
 function App() {
+    let apps = ["Google Chrome", "Brave Browser", "FireFox"];
 
-    function printUpdate(name: any) {
+    function printSearch(name: any) {
         Search(name)
     }
-    const updateName = (e: any) => printUpdate(e.target.value);
+    const UpdateSearch = (e: any) => printSearch(e.target.value);
 
     return (
         <div id="App">
             <div id="input" className="search-box">
-                <input id="name" className="search-input" onChange={updateName} name="input" type="text" spellCheck="false" placeholder="Search..." />
+                <input id="name" className="search-input" onChange={UpdateSearch} name="input" type="text" spellCheck="false" placeholder="Search..." />
             </div>
 
             <div id="results" className="results-div">
-
+                {apps.map((item, index) => (
+                    <div id="resultcard" className="result-card">
+                        <button>
+                            <img src="https://cdn.discordapp.com/attachments/759689232326328323/1136057337015185559/Google_Chrome_icon_February_2022.svg.png" alt="result icon"/>
+                            <h1>{item}</h1>
+                        </button>
+                    </div>
+                    ))}
             </div>
 
             <div id="footer" className="footer-div">
