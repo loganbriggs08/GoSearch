0fd61cad3758eb38cb5fd0ce83c2d9c3bba8973e
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-08-02 22:22:38 +0100

    Get recommended apps from the database and return them

--- modules/search/recommended.go
+++ modules/search/recommended.go
@@ -1,5 +1,26 @@
 package search
 
-func GetRecommended() []string{
+import (
+	"fmt"
+
+	"github.com/pterm/pterm"
+	"github.com/NotKatsu/GoSearch/database"
+)
+
+type RecommendedAppStruct struct {
+	Name string
+	Location string
+	Visits uint16
+}
+
+func GetRecommended() []RecommendedAppStruct{
+	recommendedApps, recommendedAppsError := database.GetRecommendedApps()
 	
+	if recommendedAppsError != nil {
+		pterm.Fatal.WithFatal(true).Println(recommendedAppsError)
+	}
+
+
+	return recommendedApps
+
 }
