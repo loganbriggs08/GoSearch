bd38ce87a50c936e5264ce1757ab19ba9d61baa2
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-08-02 20:00:02 +0100

    return a boolean value for if the database could be setup or not

--- database/database.go
+++ database/database.go
@@ -3,19 +3,27 @@ package database
 import (
 	"database/sql"
 
-	"github.com/pterm/pterm"
 	_ "github.com/mattn/go-sqlite3"
+	"github.com/pterm/pterm"
 )
 
 var database *sql.DB
 
-func SetupDatabase() {
+func SetupDatabase() bool {
 	var err error
 	database, err = sql.Open("sqlite3", "database.db")
 
 	if err != nil {
 		pterm.Fatal.WithFatal(true).Println(err)
+		return false
 	}
 
-	database.Exec("CREATE TABLE IF NOT EXISTS recommended_apps(app_name VARCHAR(50), app_location VARCHAR(255), app_visits BIGINT)")
+	_, databaseTableCreationError := database.Exec("CREATE TABLE IF NOT EXISTS recommended_apps(app_name VARCHAR(50), app_location VARCHAR(255), app_visits BIGINT)")
+
+	if databaseTableCreationError != nil {
+		pterm.Fatal.WithFatal(true).Println(err)
+		return false
+	} else {
+		return true
+	}
 }
