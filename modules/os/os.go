4eff9822aad81a933b6700b42ab1b0d0ea44d6c2
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-08-02 20:16:11 +0100

    Create os.go and add GetAppDataFolder and createAppDataFolder functions

---
+++ modules/os/os.go
@@ -0,0 +1,40 @@
+package os
+
+import (
+	"os"
+	"os/user"
+	"path/filepath"
+)
+
+func GetAppDataFolder() (string, error) {
+	currentUser, err := user.Current()
+	
+	if err != nil {
+		return "", err
+	}
+
+	appDataPath := filepath.Join(currentUser.HomeDir, "AppData", "Roaming")
+
+	return appDataPath, nil
+}
+
+func createAppDataFolder() (string, error) {
+	appDataPath, err := GetAppDataFolder()
+	
+	if err != nil {
+		return "", err
+	}
+
+	appDataFolderPath := filepath.Join(appDataPath, "GoSearch")
+
+	if _, err := os.Stat(appDataFolderPath); os.IsNotExist(err) {
+		err = os.MkdirAll(appDataFolderPath, 0755)
+		
+		if err != nil {
+			return "", err
+		}
+	}
+
+	return appDataFolderPath, nil
+}
+
