a4b2b75767d951eec6b60808a9c5916e76b820bb
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-08-02 21:30:20 +0100

    Create GetRecommendedApps to get the top 3 recommended apps

--- database/database.go
+++ database/database.go
@@ -45,6 +45,28 @@ func SetupDatabase() bool {
 	}
 }
 
-func GetRecommendedApps() {
+func GetRecommendedApps() ([]RecommendedAppStruct, error) {
+	var RecommendedAppStructArray []RecommendedAppStruct
+	rows, recommendedAppsDatabaseQueryError := database.Query("SELECT app_name, app_location, app_visits FROM recommended_apps ORDER BY app_visits DESC LIMIT 3")
 
+	if recommendedAppsDatabaseQueryError != nil {
+		pterm.Fatal.WithFatal(true).Println(recommendedAppsDatabaseQueryError)
+		return RecommendedAppStructArray, recommendedAppsDatabaseQueryError
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var currentRecommendedApp RecommendedAppStruct
+
+		rowsScanError := rows.Scan(&currentRecommendedApp.Name, &currentRecommendedApp.Location, &currentRecommendedApp.Visits)
+
+		if rowsScanError != nil {
+			pterm.Fatal.WithFatal(true).Println(rowsScanError)
+			return RecommendedAppStructArray, rowsScanError
+		}
+
+		RecommendedAppStructArray = append(RecommendedAppStructArray, currentRecommendedApp)
+	}
+
+	return RecommendedAppStructArray, nil
 }
