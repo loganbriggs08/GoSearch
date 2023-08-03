98983d848b24644a2271aa2ecca351493e803920
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-08-03 17:09:43 +0100

    Create App struct

--- modules/structs.go
+++ modules/structs.go
@@ -1,9 +1,15 @@
 package modules
 
 type RecommendedAppStruct struct {
-	Name     string
-	Location string
+	Name         string
+	Location     string
 	IconLocation string
-	Favorite bool
-	Visits   uint16
+	Favorite     bool
+	Visits       uint16
+}
+
+type App struct {
+	Name         string
+	Location     string
+	IconLocation string
 }
