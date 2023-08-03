b47d84a2b67dbcffba4ad7f098cacba1bc279e04
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-08-03 19:24:07 +0100

    Create FileReturnStruct interface

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,44 +2,47 @@ import './App.css';
 import React, { useState, useEffect } from "react";
 import { Search, HandleButtonClickEvent } from "../wailsjs/go/main/App";
 
-export interface RecommendedAppStruct {
-    Name: string;
-    Location: string;
-    Visits: number;
+export interface FileReturnStruct {
+    Name:         string;
+    Location:     string;
+    IconLocation: string;
+    Favorite:     boolean;
+    Visits:       number;
 }
 
 function App() {
-    const [results, setResults] = useState<RecommendedAppStruct[]>([]);
-
-    function fetchSearchResultsSearch(query: any) {
-        Search(query).then((response: RecommendedAppStruct[]) => {
-            if (response.length === 1 && response[0].Name === "") {
-                setResults([]);
-            } else {
-                setResults(response)
-            }
-        });
-    }
-
-    const UpdateSearch = (e: any) => fetchSearchResultsSearch(e.target.value);
+    const [results, setResults] = useState<FileReturnStruct[]>([]);
+    let debounceTimer: number | null = null;
 
     useEffect(() => {
-        UpdateSearch({ target: { value: "" } });
-        }, [])
+        fetchSearchResults("");
+        }, []);
 
-    const handleButtonClick = (result: any) => {
-        HandleButtonClickEvent(result)
+    function fetchSearchResults(query: string) {
+        if (debounceTimer !== null) {
+            clearTimeout(debounceTimer);
+        }
 
-        useEffect(() => {
-            UpdateSearch({ target: { value: "" } });
-            }, [])
+        debounceTimer = window.setTimeout(() => {
+            Search(query).then((response: FileReturnStruct[] | null) => {
+                if (response === null || (response.length === 1 && response[0].Name === "")) {
+                    setResults([]);
+                } else {
+                    setResults(response);
+                }
+            });
+            }, 1000);
+    }
 
+    const handleButtonClick = (result: FileReturnStruct) => {
+        HandleButtonClickEvent(result);
+        setResults([]);
     }
 
     return (
         <div id="App">
             <div id="input" className="search-box">
-                <input id="name" className="search-input" onChange={UpdateSearch} name="input" type="text" spellCheck="false" placeholder="Search..." />
+                <input id="name" className="search-input" onChange={(e) => fetchSearchResults(e.target.value)} name="input" type="text" spellCheck="false" placeholder="Search..." />
             </div>
 
             <div id="results" className="results-div">
@@ -56,11 +59,11 @@ function App() {
             <div id="footer" className="footer-div">
                 {results.length === 0 && <h1>No Results found</h1>}
                 {results.length === 1 && <h1>{results.length} Result found</h1>}
-                {results.length <= 3 && results.length != 0 && results.length != 1 && <h1>{results.length} Results found</h1>}
+                {results.length <= 3 && results.length !== 0 && results.length !== 1 && <h1>{results.length} Results found</h1>}
                 {results.length >= 4 && <h1>{results.length} Results found - Scroll to see all results</h1>}
             </div>
         </div>
-    );
+        );
 }
 
 export default App;
