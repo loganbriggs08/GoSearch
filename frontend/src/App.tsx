3d031023ec4841f3262ba6ed5df42ec26d35f791
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-08-05 19:00:40 +0100

    Get Current Page from app.go and then show the user that component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,24 @@
+import React, { useEffect, useState } from 'react';
 import './themes/default-theme.css';
-import SearchComponent from './Search'
-import SettingsComponent from './Settings'
-import {GetCurrentPage} from "../wailsjs/go/main/App"
+import SearchComponent from './Search';
+import SettingsComponent from './Settings';
+import { GetCurrentPage } from "../wailsjs/go/main/App";
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('');
+
+  useEffect(() => {
+    GetCurrentPage().then((currentpage: string) => {
+      setCurrentPage(currentpage);
+    });
+  }, []);
+
   return (
-    <SettingsComponent/>
-  );
+    <div>
+      {currentPage === "Search" && <SearchComponent />}
+      {currentPage === "Settings" && <SettingsComponent />}
+    </div>
+    );
 }
 
 export default App;
