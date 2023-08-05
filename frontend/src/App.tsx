6c49b1a5d65a721ba6c2f4986b89f12a82e2e956
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-08-05 18:28:05 +0100

    Import GetCurrentPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import './themes/default-theme.css';
 import SearchComponent from './Search'
 import SettingsComponent from './Settings'
+import {GetCurrentPage} from "../wailsjs/go/main/App"
 
 function App() {
   return (
