1acf5dcfb6e130c121804084b3591734a08f9514
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-08-06 21:54:21 +0100

    os.go -> machine.go

--- backend/machine/machine.go
+++ backend/machine/machine.go
@@ -1,47 +1,15 @@
-package os
+package machine
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
-	"os/user"
 	"path/filepath"
 
 	"github.com/NotKatsu/GoSearch/database"
 
 	"github.com/pterm/pterm"
 )
-
-func GetAppDataFolder() (string, error) {
-	currentUser, err := user.Current()
-
-	if err != nil {
-		return "", err
-	}
-
-	appDataPath := filepath.Join(currentUser.HomeDir, "AppData", "Roaming")
-
-	return appDataPath, nil
-}
-
-func CreateAppDataFolder() (string, error) {
-	appDataPath, err := GetAppDataFolder()
-
-	if err != nil {
-		return "", err
-	}
-
-	appDataFolderPath := filepath.Join(appDataPath, "GoSearch")
-
-	if _, err := os.Stat(appDataFolderPath); os.IsNotExist(err) {
-		err = os.MkdirAll(appDataFolderPath, 0755)
-
-		if err != nil {
-			return "", err
-		}
-	}
-
-	return appDataFolderPath, nil
-}
 
 func OpenExecutable(executablePath string) bool {
 	application := exec.Command(executablePath)
@@ -81,7 +49,7 @@ func addDataToDatabase(path string, info os.FileInfo, err error) error {
 			return err
 		}
 	}
-
+	fmt.Println(path)
 	database.InsertIntoCache(path, filepath.Base(path), filepath.Ext(path))
 	return nil
 }
