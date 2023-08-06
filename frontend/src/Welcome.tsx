582e94d1117d3547388426dd3e05ca1658711f8e
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-08-06 11:41:02 +0100

    Add Cancel and Continue button to Welcome.tsx

--- frontend/src/Welcome.tsx
+++ frontend/src/Welcome.tsx
@@ -1,4 +1,10 @@
+import {CloseApp, SetPage} from "../wailsjs/go/main/App";
+
 function WelcomeComponent() {
+
+    function closeApp() {CloseApp()}
+    function setPage(page: string) {SetPage(page)}
+
     return (
         <div id="Welcome" onContextMenu={(e) => { e.preventDefault() }}>
             <div id="header" className="header-div">
@@ -6,12 +12,17 @@ function WelcomeComponent() {
                 <p>Hello There.. Thanks for installing GoSearch, Please Read Below.</p>
                 <hr className="rounded"/>
             </div>
-            
-            <div className="main-text">
+
+            <div className="information-container">
                 <h2>Information:</h2>
-                <p>In order for you to use GoSearch we first need to cache your system so we can provide you will file results at a super speed,
-                this means we need permission from you to do this as this could take a little time. We also need to add that to keep our cache as
-                speedy as possible we need to run GoSearch in the background to keep caching your files.</p>
+                <p>In order for you to use GoSearch we first need to cache your system so we can provide you with file results at a super speed,
+                    this means we need consent from you to do this as this could take a little time. We also need to add that to keep our cache as
+                    speedy as possible we need to run GoSearch in the background to keep caching your files.</p>
+            </div>
+
+            <div className="continue-button-container">
+                <button className="cancel-button" onClick={() => closeApp()}>Cancel</button>
+                <button className="continue-button" onClick={() => setPage("Setup")}>Continue</button>
             </div>
         </div>
         );
