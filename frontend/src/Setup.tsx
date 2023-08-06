4243938efacad81e96639739607099d9e325ae3f
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-08-06 11:25:03 +0100

    Create Setup.tsx

---
+++ frontend/src/Setup.tsx
@@ -0,0 +1,30 @@
+import {CloseApp} from "../wailsjs/go/main/App";
+
+function SetupComponent() {
+
+    function closeApp() {CloseApp()}
+
+    return (
+        <div id="Welcome" onContextMenu={(e) => { e.preventDefault() }}>
+            <div id="header" className="header-div">
+                <h1>Sit Tight...</h1>
+                <p>Hello There.. Thanks for installing GoSearch, Please Read Below.</p>
+                <hr className="rounded"/>
+            </div>
+
+            <div className="information-container">
+                <h2>Information:</h2>
+                <p>In order for you to use GoSearch we first need to cache your system so we can provide you with file results at a super speed,
+                    this means we need consent from you to do this as this could take a little time. We also need to add that to keep our cache as
+                    speedy as possible we need to run GoSearch in the background to keep caching your files.</p>
+            </div>
+
+            <div className="continue-button-container">
+                <button className="cancel-button" onClick={() => closeApp()}>Cancel</button>
+                <button className="continue-button" onClick={() => console.log("contiune clicked")}>Continue</button>
+            </div>
+        </div>
+        );
+}
+
+export default SetupComponent;
