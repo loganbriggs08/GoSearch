7126e1d7107ef28cd8910fc037d3ed35085a2eb2
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-08-06 21:54:25 +0100

    Create appdata.go

---
+++ backend/appdata/appdata.go
@@ -0,0 +1,39 @@
+package appdata
+
+import (
+	"os"
+	"os/user"
+	"path/filepath"
+)
+
+func GetAppDataFolder() (string, error) {
+	currentUser, err := user.Current()
+
+	if err != nil {
+		return "", err
+	}
+
+	appDataPath := filepath.Join(currentUser.HomeDir, "AppData", "Roaming")
+
+	return appDataPath, nil
+}
+
+func CreateAppDataFolder() (string, error) {
+	appDataPath, err := GetAppDataFolder()
+
+	if err != nil {
+		return "", err
+	}
+
+	appDataFolderPath := filepath.Join(appDataPath, "GoSearch")
+
+	if _, err := os.Stat(appDataFolderPath); os.IsNotExist(err) {
+		err = os.MkdirAll(appDataFolderPath, 0755)
+
+		if err != nil {
+			return "", err
+		}
+	}
+
+	return appDataFolderPath, nil
+}
