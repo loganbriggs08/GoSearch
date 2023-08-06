3626a918a1453f460ba025df6c1b85d36e7d64a4
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-08-06 11:40:18 +0100

    Finish setup page

--- frontend/src/Setup.tsx
+++ frontend/src/Setup.tsx
@@ -7,21 +7,19 @@ function SetupComponent() {
     return (
         <div id="Welcome" onContextMenu={(e) => { e.preventDefault() }}>
             <div id="header" className="header-div">
-                <h1>Sit Tight...</h1>
-                <p>Hello There.. Thanks for installing GoSearch, Please Read Below.</p>
+                <h1>Please Sit Tight... 😊</h1>
+                <p>We are currently setting up, this could take a while so maybe get a coffee.</p>
                 <hr className="rounded"/>
             </div>
 
             <div className="information-container">
                 <h2>Information:</h2>
-                <p>In order for you to use GoSearch we first need to cache your system so we can provide you with file results at a super speed,
-                    this means we need consent from you to do this as this could take a little time. We also need to add that to keep our cache as
-                    speedy as possible we need to run GoSearch in the background to keep caching your files.</p>
-            </div>
+                <p>We are currently caching the location of all your files, this makes it easier for us to provide you
+                    with faster and more accurate search results, however this could take a little bit of time so please
+                    be patient and take your time to do something else. It is also important to add that we do not send
+                    your files anywhere and our cache stays on your system.
 
-            <div className="continue-button-container">
-                <button className="cancel-button" onClick={() => closeApp()}>Cancel</button>
-                <button className="continue-button" onClick={() => console.log("contiune clicked")}>Continue</button>
+                    <br/><br/>Please don't turn off your computer.</p>
             </div>
         </div>
         );
