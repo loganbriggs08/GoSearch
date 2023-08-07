d1034c0e75015086ecc30a740e056003d64771c2
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-08-07 22:39:01 +0100

    Create handleFileChangeEvent

--- listener/listener.go
+++ listener/listener.go
@@ -23,10 +23,24 @@ func getRootPath() (string, error) {
 }
 
 func handleFileChangeEvent(watcher *fsnotify.Watcher, done chan bool) {
+	for {
+		select {
+		case event := <-watcher.Events:
+			if event.Op&fsnotify.Write == fsnotify.Write {
+				pterm.Println(event.Name)
+			}
 
+			if event.Op&fsnotify.Write == fsnotify.Remove {
+				pterm.Println(event.Name)
+			}
+		}
+	}
+	done <- true
 }
 
 func CreateWatcher() {
+	var err error
+
 	watcher, newWatcherError := fsnotify.NewWatcher()
 
 	if newWatcherError != nil {
@@ -35,4 +49,28 @@ func CreateWatcher() {
 	defer watcher.Close()
 
 	rootPath, getRootPathError := getRootPath()
+
+	if getRootPathError != nil {
+		pterm.Fatal.WithFatal(true).Println(newWatcherError)
+	}
+
+	err = filepath.Walk(rootPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if info.IsDir() {
+			return watcher.Add(path)
+		}
+		return nil
+	})
+
+	if err != nil {
+		pterm.Fatal.WithFatal(true).Println(err)
+		return
+	}
+
+	done := make(chan bool)
+	go handleFileChangeEvent(watcher, done)
+
+	<-done
 }
