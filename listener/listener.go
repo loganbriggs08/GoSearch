fb2f87bd3e1358a8615c7c17bae6216571e99527
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-08-07 22:26:57 +0100

    Add getRootPath function

--- listener/listener.go
+++ listener/listener.go
@@ -1,11 +1,38 @@
 package listener
 
-import "github.com/fsnotify/fsnotify"
+import (
+	"os"
+	"path/filepath"
+	"runtime"
+
+	"github.com/fsnotify/fsnotify"
+	"github.com/pterm/pterm"
+)
+
+func getRootPath() (string, error) {
+	if runtime.GOOS == "windows" {
+		homeDrive := os.Getenv("HOMEDRIVE")
+		homePath := os.Getenv("HOMEPATH")
+
+		desktop := filepath.Join(homeDrive+homePath, "Desktop")
+
+		return desktop, nil
+	} else {
+		return "/", nil
+	}
+}
 
 func handleFileChangeEvent(watcher *fsnotify.Watcher, done chan bool) {
 
 }
 
 func CreateWatcher() {
+	watcher, newWatcherError := fsnotify.NewWatcher()
 
+	if newWatcherError != nil {
+		pterm.Fatal.WithFatal(true).Println(newWatcherError)
+	}
+	defer watcher.Close()
+
+	rootPath, getRootPathError := getRootPath()
 }
