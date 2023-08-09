7b199f19ea232bc0f0f17a22177a351e04ae01bd
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-08-09 20:30:23 +0100

    Update ContainsQuestionKeyword so false is not accidently returned

--- backend/search/keywords.go
+++ backend/search/keywords.go
@@ -1,19 +1,22 @@
 package search
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 var (
 	questionKeywords = []string{"what", "when", "where", "which", "who", "whom", "whose", "why", "how", "is", "are", "can", "do"}
 )
 
 func ContainsQuestionKeyword(query string) bool {
+	fmt.Println("Checking query")
 	reformedQuery := strings.ToLower(query)
 
 	for _, keyword := range questionKeywords {
+
 		if strings.HasPrefix(reformedQuery, keyword) {
 			return true
-		} else {
-			return false
 		}
 	}
 
