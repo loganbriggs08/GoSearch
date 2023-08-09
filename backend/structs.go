ccbda94c804f1697ffac8d3188ac11a523a11cbd
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-08-09 20:30:34 +0100

    Add GoogleReturnStruct

--- backend/structs.go
+++ backend/structs.go
@@ -4,6 +4,12 @@ type FileReturnStruct struct {
 	Name         string
 	Location     string
 	IconLocation string
+	Link         string
 	Favorite     bool
 	Visits       uint16
+}
+
+type GoogleReturnStruct struct {
+	Title string
+	Link  string
 }
