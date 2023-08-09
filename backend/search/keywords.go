9ee6958da4ee56a77ddb06e503173ceea9d571b1
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-08-09 18:30:34 +0100

    ContainsKeyword -> ContainsQuestionKeyword

--- backend/search/keywords.go
+++ backend/search/keywords.go
@@ -1,5 +1,21 @@
 package search
 
-func ContainsKeyword(query string) bool {
+import "strings"
 
+var (
+	questionKeywords = []string{"what", "when", "where", "which", "who", "whom", "whose", "why", "how", "is", "are", "can", "do"}
+)
+
+func ContainsQuestionKeyword(query string) bool {
+	reformedQuery := strings.ToLower(query)
+
+	for _, keyword := range questionKeywords {
+		if strings.HasPrefix(reformedQuery, keyword) {
+			return true
+		} else {
+			return false
+		}
+	}
+
+	return false
 }
